de32a033aaa9d9ca63bfdf9936ac1076395c9768
SPPEDAI <65110113+spped2000@users.noreply.github.com>
2024-07-23 21:27:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,20 @@ import streamlit as st
 import feedparser
 from bs4 import BeautifulSoup
 
+# Hide GitHub and fork badges
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Function to parse the Medium RSS feed
 def load_medium_articles(url):
     return feedparser.parse(url).entries
